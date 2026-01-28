bbba2b32e50f2cd9d973848e7e011b5d6ebdea30
Eduardo Carvalho Valadão <104527380+eduardovaladao@users.noreply.github.com>
2026-01-28 16:42:23 -0300

    Add files via upload

---
+++ CampoMinado/src/dominio/Jogador.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Jogador {
+    private int id;
+    private String apelido;
+    private String senha;
+    private List<Jogo> jogos;
+
+    public Jogador(int id, String apelido, String senha) {
+        this.id = id;
+        this.apelido = apelido;
+        this.senha = senha;
+        this.jogos = new ArrayList<>();   //criando lista//
+    }
+
+    public int getId(){ 
+        return id; 
+    }
+    public String getApelido(){ 
+        return apelido; 
+    }
+    public String getSenha(){ 
+        return senha; 
+    }
+
+    public void adicionarJogo(Jogo jogo){
+        if (jogo != null) jogos.add(jogo);
+    }
+
+    public List<Jogo> getJogos(){
+        return List.copyOf(jogos);   //você retorna uma cópia imutável//
+    }
+
+    public int getPontuacaoTotal() {
+    int soma = 0;
+    for (int i = 0; i < jogos.size(); i++){   //pega a pontuação de cada jogo que o jogador já jogou, e soma tudo, ex: 10 + 30 + 25. Pontuação total = 65//
+        soma += jogos.get(i).getPontuacao();
+    }
+    return soma;
+}
+}
+
